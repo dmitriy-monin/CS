818da921a4f11544e0639121cf0ffc6053473b55
Дмитрий <xburv@mail.ru>
2022-02-09 22:06:39 +0500

    Task

---
+++ 04 Найти максимальное из трех чисел/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти максимальное из трёх чисел
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+double c = double.Parse(Console.ReadLine());
+double max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+Console.WriteLine($"Максимальное число: {max}");
