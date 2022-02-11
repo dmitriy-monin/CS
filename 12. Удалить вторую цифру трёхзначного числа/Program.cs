afa52da5f6765426b389986ae49c5dfa1e9d623c
Дмитрий <xburv@mail.ru>
2022-02-12 00:14:20 +0500

    Исправление ошибок

--- 12. Удалить вторую цифру трёхзначного числа/Program.cs
+++ 12. Удалить вторую цифру трёхзначного числа/Program.cs
@@ -1,18 +1,5 @@
 ﻿// Удалить вторую цифру трёхзначного числа
 
-// int a = 0;
-// string s = string.Empty;
-
-// System.Console.Write("Введите трехзначное число ");
-// s = Console.ReadLine();
-// a = s.Length;
-
-// if(a == 3) 
-// {
-//     System.Console.WriteLine("Удаляем цифру " + s[s.Length-2] + ", получаем " + s[s.Length-3] + s[s.Length-1]);
-// }
-// else System.Console.WriteLine("Число не является трехзначным");
-
 Console.Clear();
 Console.WriteLine("Введите число: ");
 string valueStr = Console.ReadLine();
