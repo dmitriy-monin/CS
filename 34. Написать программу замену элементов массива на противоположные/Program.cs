47954b4e465c903832b1ecaee5c3b0b615289123
Дмитрий Монин <xburv@MacBook-Pro-Dmitrij.local>
2022-02-20 15:00:51 +0500

    Task

---
+++ 34. Написать программу замену элементов массива на противоположные/Program.cs
@@ -0,0 +1,48 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+
+// С противоположным знаком?
+
+int GetIntFromConsole()
+{
+    Console.Write("Введите число от 1 до 10: ");
+    string input1 = Console.ReadLine();
+    string error = "0";
+    if (int.TryParse(input1, out int input) && input>=1 && input<=10)
+        return input;
+    else return int.Parse(error);
+}
+
+int[] ArrayInit(int num)
+{
+    int[] array = new int[num];
+    Random  rand = new Random();
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = rand.Next(-10,10);    
+    }
+    return array;
+}
+
+void ArrayPrint(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+        
+    }
+}
+
+void ArrayPrintNeg(int[] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.Clear();
+int getInt = GetIntFromConsole();
+int[] array = ArrayInit(getInt);
+ArrayPrint(array);
+ArrayPrintNeg(array);
---
+++ 36. Задать массив заполнить случайными положительными трёхзначными числами/Program.cs
@@ -0,0 +1,22 @@
+﻿// 36. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+Console.Clear();
+int[] arr = new int[20];
+int sumPos = 0;
+int sumNeg = 0;
+
+Random rand = new Random();
+Console.Write("Создан массив: ");
+
+for (int i = 0; i < 20; i++)
+{
+    arr[i] = rand.Next(100, 1000);
+    Console.Write($"{arr[i]} ");
+    if (arr[i]%2 == 0)
+        sumPos = sumPos + 1;
+    else
+        sumNeg = sumNeg + 1;
+}
+
+Console.WriteLine("\nКоличество четных чисел = " + sumPos);
+Console.WriteLine("Количество нечетных чисел = " + sumNeg);
